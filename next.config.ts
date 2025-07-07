5f592dc150905522c38d2f443b9f2c8e941eecf2
k-bhatia2 <kshitijbhatia02@gmail.com>
2025-07-07 16:55:25 +0530

    changes made for github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Optional: Set basePath if deploying to a subdirectory
+  // basePath: '/wow_landing',
 };
 
 export default nextConfig;
